9a04ec3a21c7f9ecf9ceeb8203c8f5db6927faf5
ezekiel444 <ezekielmatomi@gmail.com>
2019-09-08 18:00:03 +0200

    part 1 requirement 2 add links

--- js/my-github.js
+++ js/my-github.js
@@ -3,18 +3,26 @@
 //fetch("https://api.github.com/users/ezekiel444/repos");
 
 console.log("Loading Github repos");
+function fetchAndShowGithubContent() {
+  let reposList = document.querySelector("#repos-list");
 
-let reposList = document.querySelector("#repos-list");
+  fetch("https://api.github.com/users/ezekiel444/repos")
+    .then(res => {
+      return res.json();
+    })
+    .then(reposarr => {
+      reposarr.forEach(repo => {
+        // create a link inside of an li and add the li to the list.
+        let newLi = document.createElement("li");
+        let createLink = document.createElement("a");
+        createLink.setAttribute("href", repo.html_url);
+        createLink.innerText = repo.name;
 
-fetch("https://api.github.com/users/ezekiel444/repos")
-  .then(res => {
-    return res.json();
-  })
-  .then(reposarr => {
-    reposarr.forEach(repo => {
-      let newLi = document.createElement("li");
-      console.log(repo.name);
-      newLi.innerText = repo.name;
-      reposList.appendChild(newLi);
+        newLi.appendChild(createLink);
+
+        reposList.appendChild(newLi);
+      });
     });
-  });
+}
+
+fetchAndShowGithubContent();
