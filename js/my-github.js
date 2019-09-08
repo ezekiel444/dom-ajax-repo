4e49ee4d066b029f97fe1a050724f79c8627d047
ezekiel444 <ezekielmatomi@gmail.com>
2019-09-08 16:59:11 +0200

    part 1 requirement 1

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,20 @@
 // Write code here to communicate with Github
+
+//fetch("https://api.github.com/users/ezekiel444/repos");
+
+console.log("Loading Github repos");
+
+let reposList = document.querySelector("#repos-list");
+
+fetch("https://api.github.com/users/ezekiel444/repos")
+  .then(res => {
+    return res.json();
+  })
+  .then(reposarr => {
+    reposarr.forEach(repo => {
+      let newLi = document.createElement("li");
+      console.log(repo.name);
+      newLi.innerText = repo.name;
+      reposList.appendChild(newLi);
+    });
+  });
